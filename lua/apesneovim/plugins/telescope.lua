9d67b1f1122c566ce9a33803e6ce1f2fc6bd27fe
YUSHACOD <ayusparanjale@gmail.com>
2024-03-31 19:44:50 +0530

    changes

--- lua/apesneovim/plugins/telescope.lua
+++ lua/apesneovim/plugins/telescope.lua
@@ -7,7 +7,6 @@ return {
 		local builtin = require('telescope.builtin')
 		-- A list of parser names, or "all" (the five listed parsers should always be installed)
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 		vim.keymap.set('n', '<leader>ps', function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") });
 		end)
