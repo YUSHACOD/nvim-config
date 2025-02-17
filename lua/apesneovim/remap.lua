e1e29d2c2413f417390f2ba90d8acf0f0350478b
Ayush Paranjale <ayushparanjale@gmail.com>
2025-02-17 22:10:24 +0530

    fix: unused remap omit

--- lua/apesneovim/remap.lua
+++ lua/apesneovim/remap.lua
@@ -34,8 +34,3 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- chmoding this file
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-
-vim.keymap.set("n", "<leader><leader>", function()
-	vim.cmd("so")
-end)
