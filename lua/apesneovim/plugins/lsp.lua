7ca3e6533aca3f5f4d98c03500fd2ced831099df
YUSHACOD <ayushparanjale@gmail.com>
2025-05-31 12:13:38 +0530

    remaps

--- lua/apesneovim/plugins/lsp.lua
+++ lua/apesneovim/plugins/lsp.lua
@@ -44,8 +44,6 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
-                "tsserver",
-                "gopls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
@@ -103,7 +101,7 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<Enter>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
